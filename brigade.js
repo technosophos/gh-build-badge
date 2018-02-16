b35aa12f104e065a5c513bed4b327a5446603b19
Matt Butcher <matt.butcher@microsoft.com>
2018-02-16 14:08:13 -0700

    Update brigade

--- brigade.js
+++ brigade.js
@@ -5,7 +5,7 @@ const chartName = "/src/charts/gh-build-badge"
 const relName = "friday-demo"
 
 events.on("exec", (e, p) => {
-  console.log("this space intentionally left blank")
+  newSlack("Sending test message <https://media.giphy.com/media/JIX9t2j0ZTN9S/giphy.gif>", p, e).run()
 });
 
 events.on("helm-delete", (e, p) => {
@@ -21,14 +21,7 @@ events.on("webhook", (e, p) => {
   console.log(e.provider)
   var slack = new Job("slack-notify", "technosophos/slack-notify:latest", ["/slack-notify"])
   var m = "Installing Helm chart"
-  slack.storage.enabled = false
-  slack.env = {
-    SLACK_WEBHOOK: p.secrets.SLACK_WEBHOOK,
-    SLACK_USERNAME: "FitBit",
-    SLACK_TITLE: "Watch Me!",
-    SLACK_MESSAGE: m
-    //SLACK_ICON: "https://a.trellocdn.com/images/ios/0307bc39ec6c9ff499c80e18c767b8b1/apple-touch-icon-152x152-precomposed.png"
-  }
+  newSlack(m, p, e)
   console.log(`installing ${chartName} into ${relName}`)
 
   var helm = new Job("helm", "lachlanevenson/k8s-helm:v2.6.1");
@@ -51,13 +44,14 @@ events.on("ReplicaSet:SuccessfulDelete", (e, p) => {
 
 function newSlack(msg, p, e) {
   var slack = new Job("slack-notify", "technosophos/slack-notify:latest", ["/slack-notify"])
-  var m = `${msg} <${hook.model.shortUrl}> <@U0RMKK605>`
+  var m = `${msg} <@U0RMKK605>`
   slack.env = {
     SLACK_WEBHOOK: p.secrets.SLACK_WEBHOOK,
-    SLACK_USERNAME: "Trello",
+    SLACK_USERNAME: "Brigade",
     SLACK_TITLE: `Handled Event ${e.type}`,
     SLACK_MESSAGE: m,
-    SLACK_ICON: "https://a.trellocdn.com/images/ios/0307bc39ec6c9ff499c80e18c767b8b1/apple-touch-icon-152x152-precomposed.png"
+    //SLACK_ICON: "https://a.trellocdn.com/images/ios/0307bc39ec6c9ff499c80e18c767b8b1/apple-touch-icon-152x152-precomposed.png"
+    SLACK_ICON: "https://brigade.sh/assets/images/brigade.png"
   }
   return slack;
 }
